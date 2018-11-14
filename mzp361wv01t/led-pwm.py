519473482aa6723a6cd0a4ac9dd6a38c8f0ea8e8
tianyoujian <6275210@qq.com>
2018-11-14 15:02:54 +0000

    modity led-pwm.py

--- mzp361wv01t/led-pwm.py
+++ mzp361wv01t/led-pwm.py
@@ -4,10 +4,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 60)
+p = GPIO.PWM(12, 3000)
 p.start(100)
 t = 0
-pwm=[0,3,8,20,35,60,200]
+pwm=[0,3,8,20,35,60,100]
 bl=5
 GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
