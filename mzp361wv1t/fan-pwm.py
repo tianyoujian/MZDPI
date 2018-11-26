1fd39205fe2e68969fad40270ecb59fb37e17cfe
tianyoujian <6275210@qq.com>
2018-11-26 17:41:00 +0800

    add mzp361wv1t dir

---
+++ mzp361wv1t/fan-pwm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python  
+
+import os
+import time
+import RPi.GPIO as GPIO  
+ 
+GPIO.setmode(GPIO.BOARD)  
+GPIO.setup(12, GPIO.OUT)  
+      
+p = GPIO.PWM(12, 150)  
+p.start(50)  
+
+try:  
+	while 1:
+		temp = os.popen('vcgencmd measure_temp').readline()
+		tempfloat = float(temp.replace('temp=','').replace('\'C\n',''))
+		print ('CPU Temperature is now %.1f Centigrade' %tempfloat)
+		if tempfloat > 60:
+			print ('CPU Temperature is too high, pls cool it down')
+		if tempfloat < 43:
+			dc=0
+		else:
+			dc=(tempfloat-43)*20
+			if dc > 100:
+				dc=100
+			p.ChangeDutyCycle(dc) 
+			print ('DutyCycle is now %.1f percent' %dc)
+		time.sleep(2)  
+except KeyboardInterrupt:
+	pass  
+p.stop()  
+GPIO.cleanup()  
---
+++ mzp361wv1t/led-pwm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 1000)
+p.start(100)
+t = 0
+pwm=[0,3,8,20,35,60,100]
+bl=5
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+try:
+ while 1:
+   if GPIO.input(37):
+     time.sleep(0.05)
+     t=0
+     #print('Input was HIGH')
+   else:
+     while 1:
+       time.sleep(0.02)
+       if GPIO.input(37):
+         break
+       else:
+         t=t+1
+       if t>3:
+         t=0
+         if bl:
+           bl=bl-1
+         else:
+           bl=5
+         p.ChangeDutyCycle(pwm[bl])
+         #print('Input was LOW')
+         while not GPIO.input(37):
+           time.sleep(0.05)
+         break
+except KeyboardInterrupt:
+  pass
+p.stop()
+GPIO.cleanup()
