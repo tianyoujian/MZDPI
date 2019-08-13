414e08660db70ead08a409177123602278f7423a
tianyoujian <6275210@qq.com>
2019-08-13 18:34:46 +0800

    modity py

--- mzp361wv1t/fan-pwm.py
+++ mzp361wv1t/fan-pwm.py
@@ -1,32 +1,33 @@
-#!/usr/bin/python  
+#!/usr/bin/python
 
 import os
 import time
-import RPi.GPIO as GPIO  
- 
-GPIO.setmode(GPIO.BOARD)  
-GPIO.setup(35, GPIO.OUT)  
-      
-p = GPIO.PWM(35, 150)  
-p.start(50)  
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
 
-try:  
+p = GPIO.PWM(19, 150)
+p.start(50)
+print ('fan running.')
+try:
 	while 1:
 		temp = os.popen('vcgencmd measure_temp').readline()
 		tempfloat = float(temp.replace('temp=','').replace('\'C\n',''))
-		print ('CPU Temperature is now %.1f Centigrade' %tempfloat)
-		if tempfloat > 60:
-			print ('CPU Temperature is too high, pls cool it down')
+		#print ('CPU Temperature is now %.1f Centigrade' %tempfloat)
+		#if tempfloat > 60:
+		#	print ('CPU Temperature is too high, pls cool it down') 
 		if tempfloat < 43:
 			dc=0
 		else:
 			dc=(tempfloat-43)*20
 			if dc > 100:
 				dc=100
-			p.ChangeDutyCycle(dc) 
-			print ('DutyCycle is now %.1f percent' %dc)
-		time.sleep(2)  
+			p.ChangeDutyCycle(dc)
+			#print ('DutyCycle is now %.1f percent' %dc)
+		time.sleep(2)
 except KeyboardInterrupt:
-	pass  
-p.stop()  
-GPIO.cleanup()  
+	pass
+p.stop()
+GPIO.cleanup()
+print ('\nfan stop.')
--- mzp361wv1t/led-pwm.py
+++ mzp361wv1t/led-pwm.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+gpio_key=26
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-p = GPIO.PWM(12, 1000)
+p = GPIO.PWM(18, 1000)
 p.start(100)
 t = 0
 pwm=[0,8,20,35,60,100]
 bl=5
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(gpio_key, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
  while 1:
-   if GPIO.input(37):
+   if GPIO.input(gpio_key):
      time.sleep(0.05)
      t=0
      #print('Input was HIGH')
    else:
      while 1:
        time.sleep(0.02)
-       if GPIO.input(37):
+       if GPIO.input(gpio_key):
          break
        else:
          t=t+1
@@ -31,7 +33,7 @@ try:
            bl=5
          p.ChangeDutyCycle(pwm[bl])
          #print('Input was LOW')
-         while not GPIO.input(37):
+         while not GPIO.input(gpio_key):
            time.sleep(0.05)
          break
 except KeyboardInterrupt:
