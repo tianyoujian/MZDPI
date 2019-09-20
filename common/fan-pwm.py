3bea7102e2b697cc79d499c912316fc86d89aaf1
tianyoujian <6275120@qq.com>
2019-09-21 09:54:37 +1000

    add fan-pwm.py

---
+++ common/fan-pwm.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+
+p = GPIO.PWM(19, 150)
+p.start(50)
+print ('fan running.')
+try:
+	while 1:
+		temp = os.popen('vcgencmd measure_temp').readline()
+		tempfloat = float(temp.replace('temp=','').replace('\'C\n',''))
+		#print ('CPU Temperature is now %.1f Centigrade' %tempfloat)
+		#if tempfloat > 60:
+		#	print ('CPU Temperature is too high, pls cool it down') 
+		if tempfloat < 45:
+			dc=0
+		else:
+			dc=(tempfloat-45)*20
+			if dc > 100:
+				dc=100
+			if dc < 20:
+				dc=20
+		p.ChangeDutyCycle(dc)
+			#print ('DutyCycle is now %.1f percent' %dc)
+		time.sleep(2)
+except KeyboardInterrupt:
+	pass
+p.stop()
+GPIO.cleanup()
+print ('\nfan stop.')
